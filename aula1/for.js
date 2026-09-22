e2cf8fe7d83684b2862d5c87484f58f9c15364c0
João Victor Oliveira <jvmdeoliveira@gmail.com>
2026-09-21 21:47:02 -0300

    add exemplos - laços de repetição

---
+++ aula1/for.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log("Tabuada de 3:");
+for (let i = 0; i <= 10; i++) {
+    //console.log("3 x " + i + " = " + 3*i);
+    console.log(`3 x ${i} = ${3*i}.`);
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
